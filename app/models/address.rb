43a4b037fcdad2a94c0a80374bc907e877660a1d
Amy Hogue <hogue.a@gmail.com>
2018-07-06 21:48:28 -0500

    Remove extra space before comma in Address#to_s

--- app/models/address.rb
+++ app/models/address.rb
@@ -14,6 +14,6 @@ class Address < ApplicationRecord
   def to_s
     # TODO: override the to_s method so that it prints out the address components as follows
     # house_number street_name street_predirection street_type street_postdirection unit_type unit_number, city, state, zip_5
-  "#{house_number} #{street_predirection} #{street_name} #{street_type} #{street_postdirection} #{unit_type} #{unit_number}, #{city}, #{state} #{zip_5}".gsub(/\s{2,}/, ' ')
+  "#{house_number} #{street_predirection} #{street_name} #{street_type} #{street_postdirection} #{unit_type} #{unit_number}, #{city}, #{state} #{zip_5}".gsub(/\s{2,}/, ' ').gsub(' ,', ',')
   end
 end
